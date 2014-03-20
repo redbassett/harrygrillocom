ad711009e47a4963e18060fb7d690930a06a88b2
Harry Grillo <redbassett@gmail.com>
2014-03-20 16:09:12 -0400

    Stuff

--- inc/main.js
+++ inc/main.js
@@ -1,3 +1,4 @@
+// marks currently scrolled-to section active in nav
 (function(){
 	$(function(){
 		nav = $('nav#mainNav')
@@ -17,3 +18,26 @@
 		});
 	});
 })();
+
+// production history
+/*(function(){
+	$(function(){
+		section = $('section#resume section#productionHistory');
+		table = section.find('table.data-display');
+		sort = section.find('ul.sort');
+		
+		// load JSON
+		(function(){
+			$.getJSON('../production-history.json', function(source){
+				
+			});
+		})();
+		
+		// bind actions to display links, do sort
+		/*(function(){
+			sort.find('a[data-display="ldesign"]').click(function(){
+				
+			})
+		})();
+	});
+})();*/
