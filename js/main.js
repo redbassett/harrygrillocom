e979fa155b0ee7a66d93096d6e5a53e2656871c4
Harry Grillo <redbassett@gmail.com>
2014-03-27 16:17:59 -0400

    added smoothScroll

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+// init smoothScroll
+(function(){
+	$(function(){
+		smoothScroll.init();
+	});
+})();
+
 // marks currently scrolled-to section active in nav
 (function(){
 	$(function(){
@@ -25,18 +32,18 @@
 		section = $('section#resume section#productionHistory');
 		table = section.find('table.data-display');
 		sort = section.find('ul.sort');
-		
+
 		// load JSON
 		(function(){
 			$.getJSON('../production-history.json', function(source){
-				
+
 			});
 		})();
-		
+
 		// bind actions to display links, do sort
 		/*(function(){
 			sort.find('a[data-display="ldesign"]').click(function(){
-				
+
 			})
 		})();
 	});
